8a8d8a74604c4365e870468afedd9ce6f0b18721
rohitaryal <erucix@gmail.com>
2025-12-21 15:52:05 +0530

    account handler class

---
+++ src/Account.ts
@@ -0,0 +1,73 @@
+import type { Session } from "./Types";
+
+export default class Account {
+    session: Session | null;
+    readonly cookie: string;
+
+    constructor(cookie: string) {
+        if (!(cookie?.trim())) {
+            throw new Error("invalid cookie")
+        }
+
+        this.session = null;
+        this.cookie = cookie;
+    }
+
+    async refresh(): Promise<Session> {
+        // Make a request with cookie
+        const response = await fetch("https://labs.google/fx/api/auth/session", {
+            method: "GET",
+            headers: {
+                cookie: this.cookie,
+            }
+        });
+
+        if (response.status != 200) {
+            console.error(await response.text())
+            throw new Error("failed to fetch session info")
+        }
+
+        let sessionPayload;
+
+        try {
+            sessionPayload = await response.json() as Session;
+        } catch (err) {
+            console.error(await response.text())
+            throw new Error("failed to parse session payload")
+        }
+
+        // Check if response is valid and has access_token
+        if (sessionPayload && !(sessionPayload.access_token?.trim())) {
+            throw new Error("session payload is missing access token")
+        }
+
+        // Assign session
+        this.session = sessionPayload;
+
+        return this.session;
+    }
+
+    isExpired(): boolean {
+        // Check if everything is ok
+        if (!this.session || !(this.session.access_token?.trim()) || !(this.session.expires)) {
+            return true;
+        }
+
+        // Get time in seconds
+        const expiryDate = new Date(this.session.expires).getTime();
+        if (!expiryDate) {
+            return true;
+        }
+
+        // is current time greater than expiry time?
+        return Date.now() > expiryDate;
+    }
+
+    toString(): string {
+        if (!this.session) {
+            return ""
+        }
+
+        return JSON.stringify(this.session)
+    }
+}
