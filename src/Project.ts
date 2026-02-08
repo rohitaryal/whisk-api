5645a924ebc77965aa0577d117c10b367807a501
rohitaryal <erucix@gmail.com>
2026-02-09 05:11:45 +0530

    feat: add scene, style and subject support

--- src/Project.ts
+++ src/Project.ts
@@ -7,6 +7,9 @@ import { Account } from "./Whisk.js";
 export class Project {
     readonly account: Account;
     readonly projectId: string;
+    readonly subjects: Media[];
+    readonly scenes: Media[];
+    readonly styles: Media[];
 
     constructor(projectId: string, account: Account) {
         if (typeof projectId !== "string" || !projectId.trim()) {
@@ -19,6 +22,9 @@ export class Project {
 
         this.projectId = projectId;
         this.account = account;
+        this.subjects = [];
+        this.scenes = [];
+        this.styles = [];
     }
 
     async generateImage(input: string | PromptConfig): Promise<Media> {
@@ -56,6 +62,94 @@ export class Project {
                 "mediaCategory": "MEDIA_CATEGORY_BOARD"
             })
         });
+
+        const img = generationResponse.imagePanels[0].generatedImages[0];
+
+        return new Media({
+            seed: img.seed,
+            prompt: img.prompt,
+            workflowId: img.workflowId ?? generationResponse.workflowId,
+            encodedMedia: img.encodedImage,
+            mediaGenerationId: img.mediaGenerationId,
+            aspectRatio: img.aspectRatio,
+            mediaType: "IMAGE",
+            model: img.imageModel,
+            account: this.account
+        });
+    }
+
+    /**
+     * Generate image but with subject, scene, style attached
+     */
+    async generateImageWithReferences(input: string | PromptConfig): Promise<Media> {
+        if (typeof input === "string") {
+            input = {
+                seed: 0,
+                prompt: input,
+                model: "IMAGEN_3_5",
+                aspectRatio: "IMAGE_ASPECT_RATIO_LANDSCAPE"
+            } as PromptConfig;
+        }
+
+        if (!input.seed) input.seed = 0;
+        if (!input.model) input.model = "IMAGEN_3_5";
+        if (!input.aspectRatio) input.aspectRatio = "IMAGE_ASPECT_RATIO_LANDSCAPE";
+        if (!input.prompt?.trim?.()) throw new Error("prompt is required");
+
+        // Check if the model selected is for image generation
+        if (!Object.values(ImageGenerationModel).includes(input.model as ImageGenerationModelType)) {
+            throw new Error(`'${input.model}': invalid image generation model provided`)
+        }
+
+        const generationResponse = await request<any>(
+            "https://aisandbox-pa.googleapis.com/v1/whisk:runImageRecipe",
+            {
+                headers: { authorization: `Bearer ${await this.account.getToken()}` },
+                body: JSON.stringify({
+                    "clientContext": {
+                        "workflowId": this.projectId,
+                        "tool": "BACKBONE",
+                    },
+                    "seed": input.seed,
+                    "imageModelSettings": {
+                        "imageModel": "GEM_PIX", // TODO: Lets experiment with this for now
+                        "aspectRatio": input.aspectRatio
+                    },
+                    "userInstruction": input.prompt,
+
+                    // Attach during image generation
+                    "recipeMediaInputs": [
+                        ...this.subjects.map(item => {
+                            return {
+                                "caption": item.prompt,
+                                "mediaInput": {
+                                    "mediaCategory": "MEDIA_CATEGORY_SUBJECT",
+                                    "mediaGenerationId": item.mediaGenerationId
+                                }
+                            }
+                        }),
+                        ...this.scenes.map(item => {
+                            return {
+                                "caption": item.prompt,
+                                "mediaInput": {
+                                    "mediaCategory": "MEDIA_CATEGORY_SCENE",
+                                    "mediaGenerationId": item.mediaGenerationId
+                                }
+                            }
+                        }),
+                        ...this.styles.map(item => {
+                            return {
+                                "caption": item.prompt,
+                                "mediaInput": {
+                                    "mediaCategory": "MEDIA_CATEGORY_STYLE",
+                                    "mediaGenerationId": item.mediaGenerationId
+                                }
+                            }
+                        })
+                    ]
+                })
+            }
+        );
 
         const img = generationResponse.imagePanels[0].generatedImages[0];
 
